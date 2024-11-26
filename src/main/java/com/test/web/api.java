cef5d075c7746a1e024cd81aec1f0e64178a47f5
youssef-khalifi <youssefkhalifi767@gmail.com>
2024-11-26 17:13:49 +0100

    .

---
+++ src/main/java/com/test/web/api.java
@@ -0,0 +1,16 @@
+package com.test.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class api {
+
+    @GetMapping
+    public String hey(){
+        return "hey";
+    }
+}
